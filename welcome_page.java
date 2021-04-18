79f5c8faa04b3aca0a92ea9f903c623c25bd5cf2
smercha1999 <57993799+smercha1999@users.noreply.github.com>
2021-04-18 14:47:30 -0400

    Renamed all the files from IntelliJ to allow the project to build.

--- welcome_page.java
+++ welcome_page.java
@@ -1,2 +1,8 @@
+import javax.swing.*;
+
 public class welcome_page {
+    private JButton booksButton;
+    private JButton audioBooksButton;
+    private JButton imagesButton;
+    private JButton videosButton;
 }
