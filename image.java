3abaf8cc87cbb4b0590a353feb9330afa0208436
clark674 <82669245+clark674@users.noreply.github.com>
2021-04-18 11:46:33 -0500

    Update image.java

--- image.java
+++ image.java
@@ -1,1 +1,38 @@
+//Author Matthew Clark
+public class Image extends Artifact{
+    private int ImageID;
+    private String Location;
+    
+    public Image (String t, String cfn, String cln, String d, int i, int ImageID, String Location)
+    {
+        super(t,cfn,cln,d,i);
+        this.ImageID = ImageID;
+        this.Location=Location;
+        
+    }
+    
+    //return and set imageID methods
+    
+    public int getImageID() {
+       return ImageID;
+    }
+    
+    public void setImageID(int ImageID) {
+        this.ImageID = ImageID;
+    }
+    
+    //return and set Location Methods
+    
+    public String getLocation() {
+        return Location;
+        
+    }
+    
+    public void setLocation(String Location) {
+        this.Location = Location;
+    }
+    
+    
+    
+}
 
