b47254341284956e55c2c9a1f0ec2df061a32ea4
yukthikasajja <52077884+yukthikasajja@users.noreply.github.com>
2021-04-18 11:25:19 -0400

    Update Ebook.java

--- ebook.java
+++ ebook.java
@@ -1,1 +1,24 @@
+public class Ebook extends Book{
+    private String link;
+    
+    public Ebook(String t, String cfn, String cln, String d, int i, String ISBN, String ISBN13, String link){
+        super (t,cfn,cln,d,i,ISBN,ISBN13);
+        this.link=link;
+    }
+
+    /**
+     * @return the link
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     * @param link the link to set
+     */
+    public void setLink(String link) {
+        this.link = link;
+    }
+    
+}
 
